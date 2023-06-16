dc033ebeb68fb4cc7a398499ce85085be3db4c19
JeremMac <macal.jerem@gmail.com>
2023-06-16 14:45:58 -0700

    almost there

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - another function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+sizeof(char);
+sizeof(int);
+sizeof(long int);
+sizeof(long long int);
+sizeof(float);
+return (0);
+}
