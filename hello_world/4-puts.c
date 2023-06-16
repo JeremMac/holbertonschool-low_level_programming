b4dda909509efbc08e2a97fff150ed356a9eb21c
JeremMac <macal.jerem@gmail.com>
2023-06-16 14:20:02 -0700

    a program

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main -this is a funcion
- *
+ * main - this is a funcion
  * Return: Always 0 (success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}	
+}
