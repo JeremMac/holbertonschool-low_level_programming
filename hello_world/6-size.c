c9d40eaecbdbce487eee521e1747ebf571b713ce
JeremMac <macal.jerem@gmail.com>
2023-06-17 06:01:15 -0700

    almost there

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-printf("size of a char: %ld", sizeof(char));
-printf("\n size of an int: %ld", sizeof(int));
-printf("\nsize of a long int: %ld", sizeof(long int));
-printf("\n size of a long long int: %ld", sizeof(long double));
-printf("\n size of a float: %ld", sizeof(float));
+printf("Size of a char: %ld", sizeof(char));
+printf("\nSize of an int: %ld", sizeof(int));
+printf("\nSize of a long int: %ld", sizeof(long int));
+printf("\nSize of a long long int: %ld", sizeof(long double));
+printf("\nSize of a float: %ld", sizeof(float));
 return (0);
 }
