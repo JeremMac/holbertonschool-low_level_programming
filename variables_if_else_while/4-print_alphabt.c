9c780f1edcbfccabc136db8bff11262125c3680a
JeremMac <macal.jerem@gmail.com>
2023-06-19 06:43:31 -0700

    deleting e and q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - the program is in here
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char al;
+for (al = 'a'; al <= 'z'; al++)
+{
+putchar(al);
+}
+if (al == 'q' && al == 'e')
+{
+al == ' ';
+}
+putchar('\n');
+return (0);
+}
