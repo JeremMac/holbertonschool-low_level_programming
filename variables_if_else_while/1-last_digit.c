9f73a4135ae9f2e2b912977b2ab5da40940cc236
JeremMac <macal.jerem@gmail.com>
2023-06-19 02:57:50 -0700

    comparing last digits

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,15 +13,15 @@ n = rand() - RAND_MAX / 2;
 last_digit = n % 10;
 if (last_digit > 5)
 {
-printf("last digit of %d is %d and is greater that 5\n", n, last_digit);
+printf("Last digit of %d is %d and is greater that 5\n", n, last_digit);
 }
 if (last_digit == 0)
 {
-printf("last digit of %d is %d and is 0", n, last_digit);
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 if (last_digit < 6 && last_digit != 0)
 {
-printf("last digit of %d is %d and is less than 6 ans not 0", n, last_digit);
+printf("Last digit of %d is %d and is less than 6 ans not 0\n", n, last_digit);
 }
 return (0);
 }
