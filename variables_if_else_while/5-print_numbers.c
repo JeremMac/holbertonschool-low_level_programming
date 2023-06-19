786763b47e26ad44777f962a075886f609548d80
JeremMac <macal.jerem@gmail.com>
2023-06-19 09:27:12 -0700

    all single digit numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - the program is in here
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 0; a <= 9; a++)
+{
+printf("%d", a);
+}
+return (0);
+}
