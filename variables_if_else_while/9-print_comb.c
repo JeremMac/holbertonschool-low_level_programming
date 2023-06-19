30282a7876f6be75f1d4cf13aea18209376926b7
JeremMac <macal.jerem@gmail.com>
2023-06-19 11:04:45 -0700

    combine numbers and punctiation

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - all is in here
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
