41d46ada5d0c643a1d2ce80b14118a3f8f5dad9f
JeremMac <macal.jerem@gmail.com>
2023-06-19 10:25:00 -0700

    display letters in reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - my main workspace
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char al;
+for (al = 'z'; al > 'a'; al++)
+{
+putchar(al);
+}
+putchar('\n');
+return (0);
+}
