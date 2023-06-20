e3e4160d0c54ab4dca7d8e48cf5739cbfa24707c
JeremMac <macal.jerem@gmail.com>
2023-06-20 10:30:57 -0700

    print an absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs- prints the absolute value of int
+ * Return: Always 0 (Success)
+ * @int: parameter of _abs
+ */
+int _abs(int)
+{
+putchar(abs(int));
+return (0);
+}
