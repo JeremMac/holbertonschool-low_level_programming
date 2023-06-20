821becdf9bc3864ae5f5ce35a1a881ef30c3e5a5
JeremMac <macal.jerem@gmail.com>
2023-06-20 02:17:37 -0700

    printing putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 /**
  *main - the main code is in here
  *Return: Always 0 (Success)
  */
 int main(void)
 {
-putchar("_putchar()")
-putchar('\n')
+_putchar('c');
+putchar('\n');
 return (0);
 }
