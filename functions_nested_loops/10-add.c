94f0eb85a914407d3f34659ae14f707283d8bb95
JeremMac <macal.jerem@gmail.com>
2023-06-20 11:49:31 -0700

    addition

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - add two int together
+ * Return: return the addition value
+ * @a: first member of add
+ * @b: second member of add
+ * int - type of a and b
+ */
+int add(int a, int b)
+{
+int c;
+a + b = c;
+return (c);
+}
