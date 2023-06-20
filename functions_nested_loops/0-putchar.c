5617fba031ce9678a6993351060c1c0612dd8c5f
JeremMac <macal.jerem@gmail.com>
2023-06-20 01:32:03 -0700

    printing putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ *main - the main code is in here
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+putchar("_putchar")
+putchar('\n')
+return (0);
+}
