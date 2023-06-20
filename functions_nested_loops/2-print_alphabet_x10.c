28650fbdc9cd8bd924c9afffc3a8bd1f4117c44c
JeremMac <macal.jerem@gmail.com>
2023-06-20 06:32:19 -0700

    printing the alphabet ten times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - where the program is
+ * Return: Always 0 (Success)
+ */
+char print_alphabet_x10(void)
+{
+char al;
+int cnt;
+al = 'a';
+cnt = 1;
+while (cnt < 10)
+{
+while (al <= 'z')
+{
+putchar(al);
+al++;
+}
+putchar('\n');
+cnt++
+}
+return (al);
+}
