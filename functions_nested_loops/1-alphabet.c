c148def35ef66db4651fd93a80ee447349bd9425
JeremMac <macal.jerem@gmail.com>
2023-06-20 05:07:28 -0700

    display the alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * print_alphabet - where the program is
+ * Return: Always 0 (Success)
+ */
+char print_alphabet(char)
+{
+char al = 'a';
+while (al <= 'z')
+{
+putchar(al);
+al = a++;
+}
+return (char);
+}
