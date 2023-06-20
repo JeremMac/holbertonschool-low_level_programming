5040d1da8771a8d3fb5d1254228cec60f6fdc06c
JeremMac <macal.jerem@gmail.com>
2023-06-20 09:39:40 -0700

    checking for alphabethics characters

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * Return: 1 c is a letter
+ * @c: is a member of _isalpha
+ */
+int _isalpha(int c)
+{
+if (isalpha(c) != 0)
+{
+return (1);
+}
+else
+{
+return(0);
+}
+}
