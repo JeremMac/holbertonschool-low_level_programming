1641ec6d35d608369221a5a1dfb0348fdb35313a
JeremMac <macal.jerem@gmail.com>
2023-06-22 11:34:28 -0700

    print all number from n to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,19 +7,7 @@
  */
 void print_to_98(int n)
 {
-	if(n != 98)
-	{
-		while (n >= 98)
-		{
-			printf("%d", n);
-			if (n > 98)
-			{
-				printf(", ");
-			}
-			n--;
-		}
-	}
-	if(n != 98)
+	if (n != 98)
 	{
 		while (n <= 98)
 		{
@@ -29,7 +17,17 @@ void print_to_98(int n)
 				printf(", ");
 			}
 			n++;
-		}			   
+		}
+	}
+	else if (n != 98)
+	{
+		while (n >= 98)
+			printf("%d", n);
+		if (n > 98)
+		{
+			printf(", ");
+		}
+		n--;
 	}
 	printf("\n");
 }
