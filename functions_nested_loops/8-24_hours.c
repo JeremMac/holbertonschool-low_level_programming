0f8510d3144eb8cb8b1d347c0c3fb00c52cc9a1f
JeremMac <macal.jerem@gmail.com>
2023-06-22 11:59:52 -0700

    23 min timer

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,43 +6,44 @@
  */
 void jack_bauer(void)
 {
-int compt;
-int min_;
-int min;
-int sec_;
-int sec;compt = 0;
-min_ = 48;
-min = 0;
-sec_ = 0;
-sec = 0;
-while (compt <= 1439)
-{
-while (sec_ <= 9)
-{
-sec_++;
-if (sec_ == 10)
-{
-sec++;
-sec_ = 0;
+	int compt;
+	int min_;
+	int min;
+	int sec_;
+	int sec;
+
+	compt = 0;
+	min_ = 48;
+	min = 0;
+	sec_ = 0;
+	sec = 0;
+	while ((min_ == 2 && min == 3)&&(sec == 5 && sec_ == 9))
+	{
+		while (sec_ <= 9)
+		{
+			sec_++;
+			if (sec_ == 10)
+			{
+				sec++;
+				sec_ = 0;
+			}
+			if (sec == 6)
+			{
+				min++;
+				sec = 0;
+			}
+			if (min == 10)
+			{
+				min_++;
+				min = 0;
+			}
+			_putchar(min_);
+			_putchar(min + '0');
+			_putchar(':');
+			_putchar(sec + '0');
+			_putchar(sec_ + '0');
+			_putchar('\n');
+		}
+	}
 }
-if (sec == 6)
-{
-min++;
-sec = 0;
-}
-if (min == 10)
-{
-min_++;
-min = 0;
-}
-_putchar(min_);
-_putchar(min + '0');
-_putchar(':');
-_putchar(sec + '0');
-_putchar(sec_ + '0');
-_putchar('\n');
-}
-compt++;
-}
-}
-}
+
