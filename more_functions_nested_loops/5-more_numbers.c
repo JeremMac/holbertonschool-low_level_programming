b4adde2a778de736e9aac093aaeae9813b396a96
JeremMac <macal.jerem@gmail.com>
2023-06-23 09:36:55 -0700

    prints number from 0 to 14 ten times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers from  0 to 14 ten times
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int cnt;
+	int num;
+
+	num = 0;
+
+	while (cnt <= 10)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			_putchar(num + '0');
+		}
+		cnt++;
+	}
+}
