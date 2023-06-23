396d7b107237bac408f9e3c55bce9f16ce6e8290
JeremMac <macal.jerem@gmail.com>
2023-06-23 05:41:22 -0700

    checking for uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - check for upper case
+ * Return: return 1 if uppercase
+ */
+int _isupper(int c)
+{
+	if (n >= 65)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}	
+}
