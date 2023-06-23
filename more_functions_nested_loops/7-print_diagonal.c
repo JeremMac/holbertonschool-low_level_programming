ea391a627606f0b2e201894c5560734b26af4154
JeremMac <macal.jerem@gmail.com>
2023-06-23 13:12:50 -0700

    printing a diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,16 +7,18 @@
 void print_diagonal(int n)
 {
 	int hauteur;
+	int a
 
 	hauteur = 0;
 
 	while (hauteur < n)
 	{
+		for (a = 0; a < n; a++)
+		{
 		_putchar(' ');
 		_putchar('\\');
-		_putchar(' ');
 		_putchar('\n');
-		_putchar(' ');
+		}
 		hauteur++;
 	}
 }
