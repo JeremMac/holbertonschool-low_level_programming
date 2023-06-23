c8135bcf04c4b6acf417c853c0758693cc59ab0a
JeremMac <macal.jerem@gmail.com>
2023-06-23 09:20:52 -0700

    prints all numbers except 2 and 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
  *  *main - the program is in here
  *   *Return: Always 0 (Success)
  *    */
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 	int num;
 	for (num = 0; num <= 9; num++)
