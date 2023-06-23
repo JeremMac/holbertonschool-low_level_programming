27f67667a77c16871accd46626bb57cbf47f190e
JeremMac <macal.jerem@gmail.com>
2023-06-23 07:28:18 -0700

    function print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ * Return: Always 0 (Success)
+ */
+void print_numbers
+{
+	int num
+	
+	num = 9; 
+	
+	while (num <= 9)
+	{
+		_putchar(num + '0');
+		num++;
+	}
+	return (0);
+}
