490cf85ee3e7806fed1e1004cd37f138883ab880
JeremMac <macal.jerem@gmail.com>
2023-06-23 09:16:09 -0700

    prints all numbers except 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+nclude <stdio.h>
+/**
+ *  *main - the program is in here
+ *   *Return: Always 0 (Success)
+ *    */
+int main(void)
+{
+	int al;
+	for (al = 0; al <= 9; al++)
+	{
+		if (al != 2 && al != 2)
+		{
+			_putchar(al + '0');
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
