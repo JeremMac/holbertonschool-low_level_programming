15332418e89fa35fd6a4dc11054acfce374fbe7d
JeremMac <macal.jerem@gmail.com>
2023-06-23 07:12:48 -0700

    multipliying two int

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -3,11 +3,12 @@
  * mul - multiplies two int together
  * Return: return the result
  * @a: member of mul
- * @b second member of mul
+ * @b: second member of mul
  */
 int mul(int a, int b)
 {
 	int c;
+
 	c = a * b;
 	return (c);
 }
