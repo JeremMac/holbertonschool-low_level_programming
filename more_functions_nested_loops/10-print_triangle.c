5892d864d7e97358b72698262946b187066f070f
JeremMac <macal.jerem@gmail.com>
2023-06-24 02:42:14 -0700

    print a triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -3,20 +3,20 @@
  * print_triangle - prints a triangle
  * int - type of size
  * @size: member of print_triangle
- *Return: Always 0 (Success) 
+ *Return: Always 0 (Success)
  */
 void print_triangle(int size)
 {
 	int spc;
 	int tgl;
 
-	for(spc = size; spc == size; spc--)
+	for (spc = size; spc == size; spc--)
 	{
 		_putchar(' ');
+		_putchar('\n');
 		for (tgl = 0; tgl < size; tgl++)
 		{
 			_putchar('#');
 		}
-		_putchar('\n');
 	}
 }
