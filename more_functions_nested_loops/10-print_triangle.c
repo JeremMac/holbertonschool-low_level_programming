7000c66e444e2e1cfffaf715e2fa8f5330536080
JeremMac <macal.jerem@gmail.com>
2023-06-24 02:38:42 -0700

    print a triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle
+ * int - type of size
+ * @size: member of print_triangle
+ *Return: Always 0 (Success) 
+ */
+void print_triangle(int size)
+{
+	int spc;
+	int tgl;
+
+	for(spc = size; spc == size; spc--)
+	{
+		_putchar(' ');
+		for (tgl = 0; tgl < size; tgl++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
