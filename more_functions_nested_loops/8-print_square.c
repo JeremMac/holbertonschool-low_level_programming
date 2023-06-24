cca973dc550ba8dee5c18a43967e9bf0cba4435d
JeremMac <macal.jerem@gmail.com>
2023-06-24 01:54:28 -0700

    print a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - print a square
+ * int - type of size
+ * @size: member of print_square
+ * Return: Always 0 (Success)
+ */
+void print_square(int size)
+{
+	int prt;
+	int sqr;
+
+	for (prt = 0; prt < size; prt++)
+	{
+		_putchar('\n');
+		for (sqr = 0; sqr < size; sqr++)
+		{
+			_putchar("#");
+		}
+	}
+}
