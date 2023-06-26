dadb574ceae18f93feb22218620624c25aa88098
JeremMac <macal.jerem@gmail.com>
2023-06-26 01:47:57 -0700

    reset n to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - changes the value of n to 98
+ * Return: Always 0 (Success)
+ * int - the type of n
+ * @*n: member of reset_to_98
+ */
+void reset_to_98(int *n)
+{
+	int *point;
+	int *point = &n;
+	int *point = 98;
+	_putchar(*point + '0');
+}
