95d87de792f4f2572871d78bfc55fe9640660e01
JeremMac <macal.jerem@gmail.com>
2023-06-26 06:24:41 -0700

    returning the lenght of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - returns the lenght of a string
+ * Return: return the number of characters found
+ * char - type of *s
+ * @s: member of _strlen
+ */
+int _strlen(char *s)
+{
+	int cnt;
+
+	cnt = 0;
+
+	while (cnt < s)
+	{
+		_putchar(cnt); 
+		cnt++;
+	}
+}
