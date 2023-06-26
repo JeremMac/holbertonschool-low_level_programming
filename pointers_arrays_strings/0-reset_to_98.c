59754ac21b176c660dbd097592975fec98c6feeb
JeremMac <macal.jerem@gmail.com>
2023-06-26 02:16:21 -0700

    reset n to 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
  * reset_to_98 - changes the value of n to 98
  * Return: Always 0 (Success)
  * int - the type of n
- * @*n: member of reset_to_98
+ * @n: member of reset_to_98
  */
 void reset_to_98(int *n)
 {
