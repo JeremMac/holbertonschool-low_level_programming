ed69847abbb4eab79639580a1e3277018161ee35
JeremMac <macal.jerem@gmail.com>
2023-06-27 03:00:47 -0700

    print a string in reverse

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - reverse the content of a string
+ * Return: Always 0 (Success)
+ * char - type of *s
+ * @s: memeber of rev_string
+ */
+void rev_string(char *s)
+{
+	int cnt;
+
+	cnt = 0;
+
+	while (s[cnt] != '\0')
+	{
+		cnt++;
+	}
+	cnt--;
+	while (s[cnt] <= 0)
+	{
+		_putchar(cnt);
+		cnt--;
+	}
+}
