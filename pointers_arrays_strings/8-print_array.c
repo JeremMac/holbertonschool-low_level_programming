39de55e5120c66ed1a5de56332c97d8624bc8b88
JeremMac <macal.jerem@gmail.com>
2023-06-28 07:25:08 -0700

    printing a n number of elements in an array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - print n element of an array
+ * Return: nothing
+ * int _ type of *a and n
+ * @a: member of print_array
+ * @n: second member of print_array
+ */
+void print_array(int *a, int n)
+{
+	int len = 0;
+
+	while (a[len] < n)
+	{
+		_putchar(a[len]);
+		_putchar('\n');
+		_putchar(' ');
+		len++;
+	}
+}
