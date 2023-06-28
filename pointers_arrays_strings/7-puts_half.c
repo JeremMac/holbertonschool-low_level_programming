8df5e6a3c81994974aefed455e199696ce44e2a2
JeremMac <macal.jerem@gmail.com>
2023-06-28 04:50:29 -0700

    prints half of a chain

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -8,22 +8,38 @@ void puts_half(char *str)
 {
 	int cnt;
 	int numb;
+	int nb;
+	int n;
+	int chf;
 
 	cnt = 0;
 	numb = 0;
+	nb = 0;
+	chf = 0;
 
 	while (str[cnt] != '\0')
 	{
 		cnt++;
 		numb++;
+		nb++;
 	}
 	cnt = cnt / 2;
 	numb--;
+	n = (nb - 1) / 2; 
 
 	while (cnt <= numb)
 	{
 		_putchar(str[cnt]);
 		cnt++;
+	}
+	if (nb % 2 != 0)
+	{
+		while (chf < n)
+		{
+			_putchar(str[chf]);
+			chf++;
+		}
+
 	}
 	_putchar('\n');
 }
