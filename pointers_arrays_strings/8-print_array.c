042da8eee4f625be41cf880f024f6f92dfcb23d5
JeremMac <macal.jerem@gmail.com>
2023-06-28 10:31:17 -0700

    printing a n number of elements in an array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -14,11 +14,9 @@ void print_array(int *a, int n)
 	for (len = 0; len < n; len++)
 	{
 		printf("%d", a[len]);
-		while (len < n - 1)
+		if (len < n;)
 		{
-			_putchar(',');
-			_putchar(' ');
-			len++;
+			printf(", ");
 		}
 	}
 }
