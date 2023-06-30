70964a3599e0d492be65c9835f361fcea226bca7
JeremMac <macal.jerem@gmail.com>
2023-06-30 00:49:32 -0700

    concatanation of two strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strcat - concatanate a string at the end of another string
+ * Return: return value of dest
+ * char - type of *dest end *src
+ * @dest: pointer to dest
+ * @src: pointer to src
+ */
+char *_strcat(char *dest, char *src)
+{
+	int lendest = 0;
+	int lensrc = 0;
+	int cnt = 0;
+
+	while (dest[lendest] != '\0')
+	{
+		lendest++;
+	}
+
+	while (src[] == '\0')
+	{
+		lensrc++;
+	}
+
+	while (cnt <= lensrc)
+	{
+		dest[lendest] + src[cnt]
+		cnt++;
+	}
+	return (*dest);
+}
