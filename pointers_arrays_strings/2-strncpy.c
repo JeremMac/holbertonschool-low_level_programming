d54ceaf93841d2aa054a3e85c91d28f974e2787b
JeremMac <macal.jerem@gmail.com>
2023-06-30 05:59:19 -0700

    function that copy a string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - copy the content of a strinf
+ * Return: the copied string
+ * char _ type of *dest and *src
+ * int - type of n
+ * @dest: pointer to dest
+ * @src: pointer to src
+ * @n: member of _strncpy
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len = 0;
+
+	while (src[len] != '\0')
+	{
+		dest[len] = src[len]
+		len++;
+	}
+	return (dest);
+
+}
