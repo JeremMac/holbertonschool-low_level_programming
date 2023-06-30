f36fa7fee9efbb9c0ac1712920e59788c35f39b5
JeremMac <macal.jerem@gmail.com>
2023-06-30 07:28:50 -0700

    change a lowercase in uppercase in a string

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - changes lowercases letters to upper case
+ * Return: returns value of str
+ * @str: member of string_toupper
+ * char - type of *str
+ */
+char *string_toupper(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		if(str[len] >= 'a' && str[len] <= 'z')
+		{
+			str[len] -= 32;
+		}
+		len++;
+	}
+	return (str);
+}
