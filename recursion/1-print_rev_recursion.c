b1332a9eba0d8a6df3786c75ff597ab5c954ee2e
JeremMac <macal.jerem@gmail.com>
2023-07-03 07:04:07 -0700

    print a string in reverse

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print the characters of a string in reverse
+ * char -indicate that *s is a pointer to a char
+ * @s: is a pointer to a given string of character
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	s++;
+	_print_rev_recursion(s--);
+	_putchar(*s);
+}
