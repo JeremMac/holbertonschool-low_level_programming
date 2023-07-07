ddb78a5ebdcb75482d91d10a80b48a71cab16f24
JeremMac <macal.jerem@gmail.com>
2023-07-07 05:15:11 -0700

    printing all the arguments received

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments received
+ * @argc: indicates the number of arguments
+ * @argv: array containing the arguments
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int cnt = 0;
+	while (cnt <= argc)
+	{
+		printf("%s\n", argv[cnt]);
+		cnt++;
+	}
+	return (0);
+}
