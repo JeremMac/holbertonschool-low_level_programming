326c9b5622010819451dca6d364a56f01ba073dc
JeremMac <macal.jerem@gmail.com>
2023-07-10 07:30:03 -0700

    concatanate strings with malloc

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - concatanate the content of two given strings
+ * @s1: the first part of the string to concatanated
+ * @s2: the second part of the string to be concatanated
+ * Return: return the content of str if str != NULL
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int ls1 = 0;
+	int ls2 = 0;
+	int cnt = 0;
+	int rst = 0;
+	int true_len;
+	char *str;
+
+	while (s1[ls1] != '\0')
+	{
+		ls1++;
+	}
+	while (s2[ls2] != '\0')
+	{
+		ls2++;
+	}
+	true_len = ls1 + ls2;
+	str = malloc(true_len * sizeof(char) + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (cnt < ls1)
+	{
+		str[cnt] += s1[cnt];
+		cnt++;
+	}
+	while (cnt < true_len)
+	{
+		str[cnt] += s2[rst];
+		cnt++;
+		rst++;
+	}
+	return (str);
+}
