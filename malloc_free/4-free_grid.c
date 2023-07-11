5173ace2510705026ff63f344045b5db4688135a
JeremMac <macal.jerem@gmail.com>
2023-07-11 07:38:09 -0700

    freeing a two dimensional grid

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+/**
+ * free_grid - free the memory used by grid
+ * @grid: a pointer to the fuction we need
+ * to free the memory of
+ * @height: the given height of the array of the function.
+ */
 void free_grid(int **grid, int height)
 {
 	int cnt;
-	for(cnt = 0; cnt < height; cnt++)
+
+	for (cnt = 0; cnt < height; cnt++)
 	{
 		free(grid[cnt]);
 	}
