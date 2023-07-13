1cae4ad6447036429890f7ce4949f2c5fbf3528e
JeremMac <macal.jerem@gmail.com>
2023-07-13 05:25:08 -0700

    allocating memory for an array

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,23 +10,25 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 
-	int *ar;
+	char *ar;
 	unsigned int idx2;
 
-	if (nmemb == 0 || size == 0)
+	if ((nmemb == 0) || (size == 0))
 	{
 		return (NULL);
 	}
 
-	ar = malloc(nmemb * sizeof(size));
+	ar = malloc(nmemb * size);
 
 	if (ar == NULL)
 	{
 		return (NULL);
 	}
-	for (idx2 = 0; idx2 < nmemb; idx2++)
+
+	for (idx2 = 0; idx2 < nmemb * size; idx2++)
 	{
 		ar[idx2] = 0;
 	}
+
 	return (ar);
 }
