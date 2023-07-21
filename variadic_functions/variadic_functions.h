158d7b5e1a68d1b8092560725531b46741854449
JeremMac <macal.jerem@gmail.com>
2023-07-21 09:25:59 -0700

    header file with my variadic functions prototypes

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,13 +1,31 @@
-#ifndef __VARIADIC__FUNCTIONS__H__
-#define  __VARIADIC__FUNCTIONS__H__
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
 
+/**
+ * struct all - structure for print_all
+ * @str: take the character
+ * @f: The function associated
+ */
+
+typedef struct all
+{
+	char *str;
+	void (*f)(va_list);
+} all_t;
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
-
 void print_numbers(const char *separator, const unsigned int n, ...);
-
 void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
 
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
 
 #endif
