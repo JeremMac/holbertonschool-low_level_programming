55b7d21fd6b9cd037044d8831262f7c9ff3eb1a5
JeremMac <macal.jerem@gmail.com>
2023-08-01 09:39:47 -0700

    a function that frees the allocated memory in our linked list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -3,13 +3,18 @@
 #include "lists.h"
 
 /**
- *
+ * free_list - a function to free
+ * the allocated memory in a linked
+ * list
+ * @head: a pointer to the
+ * linked list we want to free
+ * Return: nothing
  */
 
 void free_list(list_t *head)
-{ 
+{
 
-	list_t temp = NULL;
+	list_t *temp = NULL;
 
 	while (head != NULL)
 	{
