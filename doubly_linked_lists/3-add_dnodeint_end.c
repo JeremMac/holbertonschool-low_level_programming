2781e4cfc6d205d7acbd1b592494e83a99c1c9db
JeremMac <macal.jerem@gmail.com>
2023-08-02 06:51:12 -0700

    a function to create nodes backward in a double linked list

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - a function that
+ * adds a nobe in a double linked list backward
+ * @head: pointer to a double linked list
+ * @n: the element we insert in the nodes
+ * Return: the new node created
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node_end = NULL;
+	dlistint_t *temp = NULL;
+
+	temp = *head;
+
+	new_node_end = malloc(sizeof(dlistint_t));
+
+	if (new_node_end == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node_end->n = n;
+	new_node_end->prev = *head;
+	new_node_end->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node_end;
+		new_node_end->prev = NULL; 
+		return (*head);
+	}
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+		temp->prev = new_node_end;
+	}
+
+	temp->next = new_node_end;
+	new_node_end = new_node_end->prev;
+
+	return (new_node_end);
+}
