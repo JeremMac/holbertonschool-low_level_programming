4c3a5e9bdd2cbca1e7004f6ef01af32923ce5c06
JeremMac <macal.jerem@gmail.com>
2023-08-03 09:58:49 -0700

    a function to add a new node at a given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -43,8 +43,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new_node->prev = temp;
 			new_node->next = temp->next;
 			temp->next = new_node;
+			return (new_node);
 		}
 		temp = temp->next;
 	}
-	return (new_node);
+	if (count > idx)
+	{
+		return (NULL);
+	}
+	return (NULL);
 }
