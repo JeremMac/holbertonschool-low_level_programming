87a3fe0885cf9e9160705951504ca9d95d50d1e9
JeremMac <macal.jerem@gmail.com>
2023-08-08 07:32:55 -0700

    a function that create a copy of a file

---
+++ file_io/3-cp.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * close_it - a function that
+ * checks if a close function
+ * value is -1 and prints
+ * an error
+ * @a: the variable we needs
+ * to close
+ * Return: nothing
+ */
+
+void close_it(int a)
+{
+	int b = 0;
+
+	b = close(a);
+	if (b == -1)
+	{
+		printf("Error: can't close fd %d", a);
+		exit(100);
+	}
+}
+
+
+/**
+ * main - a function that
+ * copy the content of a
+ * file into another created
+ * file
+ * @argc: number of arguments
+ * passed to main
+ * @argv: an array containing
+ * all argument passed to main
+ * Return: exit the program
+ * if there is an error
+ * otherwhise return 1
+ */
+
+int main(int argc, char *argv[])
+{
+	char *file_from = argv[1], *file_to = argv[2];
+	int o = 0, o2 = 0, r = 0, w = 0;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		printf("Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	o = open(file_from, O_RDONLY);
+	if (o == -1)
+	{
+		printf("Error: can't read from file %s\n", file_from);
+	}
+	r = read(o, buffer, sizeof(buffer));
+	if (r == -1)
+	{
+		printf("Error: can't read from %s\n", file_from);
+		close_it(o);
+		exit(98);
+	}
+
+	o2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (o2 == -1)
+	{
+		printf("Error: can't open or create file %s", file_to);
+	}
+	w = write(o2, buffer, r);
+	if (w == -1)
+	{
+		close_it(o2);
+		close_it(o);
+		printf("Error: can't write to %s\n", file_to);
+		exit(99);
+	}
+	close_it(o);
+	close_it(o2);
+	return (1);
+}
+
