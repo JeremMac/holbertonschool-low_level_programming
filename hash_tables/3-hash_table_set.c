4829c732579ff2973368d167a7acddc3ef624155
JeremMac <macal.jerem@gmail.com>
2023-08-10 07:32:29 -0700

    fixing betty and malloc

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -4,6 +4,17 @@
 #include "hash_tables.h"
 
 /**
+ * hash_table_set - a function
+ * that adds an element to the
+ * hash table
+ * @ht: a pointer to the
+ * hash table
+ * @key: the index
+ * we use to locate the node
+ * @value: the value we want
+ * to assign to the node
+ * Return: 0 if there is an error
+ * otherwise 0
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
@@ -12,16 +23,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	unsigned long int index;
 
-	index = key_index((const unsigned char *) key, ht->size);
+	if (ht == NULL)
+	{
+		return (0);
+	}
 
 	if (key == NULL || key[0] == '\0')
 	{
 		return (0);
 	}
-	if (ht == NULL)
-	{
-		return (0);
-	}
+
+	index = key_index((const unsigned char *) key, ht->size);
 
 	temp = ht->array[index];
 
@@ -35,7 +47,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		temp = temp->next;
 	}
-	new_node = malloc(sizeof(hash_node_t) * ht->size);
+	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
 		return (0);
