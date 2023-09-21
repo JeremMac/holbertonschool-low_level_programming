e3937e8e6ee131fcc707855a113eba937b132443
JeremMac <macal.jerem@gmail.com>
2023-09-21 13:17:50 +0200

    Fix: betty coding style for header file.

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,9 +1,7 @@
 #ifndef __SEARCH__ALGOS__H__
 #define   __SEARCH__ALGOS__H__
-
 #include <stdio.h>
 #include <stdlib.h>
-
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
