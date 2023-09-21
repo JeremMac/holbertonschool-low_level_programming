38816466eea9ddaf4bb02ca0d5403eda1f284bdc
JeremMac <macal.jerem@gmail.com>
2023-09-21 13:21:49 +0200

    Fix: betty style.

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -2,8 +2,6 @@
 #define   __SEARCH__ALGOS__H__
 #include <stdio.h>
 #include <stdlib.h>
-
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
 #endif
